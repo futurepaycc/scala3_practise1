5f7cf154d6d6896627caf053f83d1b98ebc45e4a
liunix <liuyifan.com@gmail.com>
2022-09-03 20:55:56 +0800

    tidy code

--- src/main/scala/Tree1.scala
+++ src/main/scala/Tree1.scala
@@ -3,6 +3,7 @@ sealed trait BinTree[+A]
 case object Leaf extends BinTree[Nothing]
 case class Branch[A](value: A, left: BinTree[A], right: BinTree[A]) extends BinTree[A]
 
+// 等号右边都已在函数内(也可看作一个大号的表达式)
 def buildTree[A](list: List[A]): BinTree[A] = list match {
   case Nil => Leaf
   case x :: xs => {
@@ -13,7 +14,7 @@ def buildTree[A](list: List[A]): BinTree[A] = list match {
 
 @main def Tree1={
 
-  // 构建二叉树
+  // 构建二叉树, 从左到右，自顶向下
   val list1 = (1 to 9).toList //range to list
   val tree1 = buildTree(list1)
   println(tree1)    
